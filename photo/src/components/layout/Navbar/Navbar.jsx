2a431c1b6052071a49817110fb7a40c6ec267f90
Pranil <pranilsinghamahapatra4@gmail.com>
2026-06-25 18:48:45 +0530

    a

--- photo/src/components/layout/Navbar/Navbar.jsx
+++ photo/src/components/layout/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Navbar.css";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -19,11 +20,51 @@ function Navbar() {
 
         <div className="nav-links">
           <ul>
-            <li>Home</li>
-            <li>Portfolio</li>
-            <li>Services</li>
-            <li>About</li>
-            <li>Contact</li>
+            <motion.li
+initial={{opacity:0,x:50}}
+animate={{opacity:1,x:0}}
+transition={{delay:.15}}
+>
+
+Home
+
+</motion.li>
+            <motion.li
+initial={{opacity:0,x:50}}
+animate={{opacity:1,x:0}}
+transition={{delay:.25}}
+>
+
+Stories
+
+</motion.li>
+           <motion.li
+initial={{opacity:0,x:50}}
+animate={{opacity:1,x:0}}
+transition={{delay:.35}}
+>
+
+Services
+
+</motion.li>
+           <motion.li
+initial={{opacity:0,x:50}}
+animate={{opacity:1,x:0}}
+transition={{delay:.45}}
+>
+
+About
+
+</motion.li>
+            <motion.li
+initial={{opacity:0,x:50}}
+animate={{opacity:1,x:0}}
+transition={{delay:.55}}
+>
+
+Contact
+
+</motion.li>
           </ul>
         </div>
 
@@ -35,11 +76,43 @@ function Navbar() {
 </div>
 
 <div
-    className="menu-icon"
-    onClick={() => setMenuOpen(!menuOpen)}
+  className="menu-icon"
+  onClick={() => setMenuOpen(!menuOpen)}
 >
-    <HiOutlineMenuAlt3 />
+  <HiOutlineMenuAlt3 />
 </div>
+
+<AnimatePresence>
+  {menuOpen && (
+    <motion.div
+      className="mobile-menu"
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100%" }}
+      transition={{
+        duration: 0.45,
+        ease: [0.22, 1, 0.36, 1],
+      }}
+    >
+      <div
+        className="close-icon"
+        onClick={() => setMenuOpen(false)}
+      >
+        <HiOutlineX />
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>Stories</li>
+        <li>Services</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <button>Book Your Story</button>
+    </motion.div>
+  )}
+</AnimatePresence>
 
       </nav>
     </header>
