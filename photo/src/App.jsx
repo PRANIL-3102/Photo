c454f21b8b18e0c1dda0d1ad5a3745cb7f0f9a91
Pranil <pranilsinghamahapatra4@gmail.com>
2026-06-30 13:56:06 +0530

    sdd

--- photo/src/App.jsx
+++ photo/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/layout/Navbar/Navbar";
 import Hero from "./components/home/Hero/Hero";
+import FeaturedStory from "./components/home/FeaturedStory/FeaturedStory";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <FeaturedStory />
     </>
   );
 }
---
+++ photo/src/components/home/FeaturedStory/FeaturedStory.jsx
@@ -0,0 +1,46 @@
+import "./FeaturedStory.css";
+
+function FeaturedStory() {
+  return (
+    <section className="featured-story">
+
+      <div className="story-container">
+
+        <div className="story-image">
+
+        </div>
+
+        <div className="story-content">
+
+          <p className="story-label">
+            FEATURED STORY
+          </p>
+
+          <h2>
+            Ayan & Sneha
+          </h2>
+
+          <h3>
+            A Wedding to Remember
+          </h3>
+
+          <p className="story-description">
+            Every wedding tells a different story.
+            From heartfelt smiles to unforgettable emotions,
+            every frame preserves a memory that lasts forever.
+          </p>
+
+<a href="#" className="story-link">
+    View Full Story
+    <span>→</span>
+</a>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default FeaturedStory;
